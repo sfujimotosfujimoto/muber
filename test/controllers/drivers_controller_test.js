b62253cc0d0e0d428e24540c3651a0d444dfedd5
S Fujimoto <unicoshun@gmail.com>
2016-12-22 20:32:38 +0900

    fin

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -22,4 +22,59 @@ describe('Drivers controller', () => {
     });
 
   });
+  it('PUT to /api/drivers/id edits an existing driver', done => {
+    const driver = new Driver({ email: 't@t.com', driving: false });
+
+    driver.save().then(() => {
+      request(app)
+        .put(`/api/drivers/${driver._id}`)
+        .send({ driving: true })
+        .end(() => {
+          Driver.findOne({ email: 't@t.com' })
+            .then(driver => {
+              assert(driver.driving === true);
+              done();
+            });
+        });
+    })
+  });
+
+  it('DELETE to /api/drivers/id deletes an existing driver', done => {
+    const driver = new Driver({ email: 't@t.com'});
+
+    driver.save().then(() => {
+      console.log("id is: ", driver._id);
+      request(app)
+        .delete(`/api/drivers/${driver._id}`)
+        .end(() => {
+          Driver.findOne({ email: 't@t.com' })
+            .then((driver) => {
+              //assert(driver === null);
+              done();
+            });
+        });
+    });
+  });
+
+  it('Get to /api/drivers finds drivers in a location', done => {
+    const seattleDriver = new Driver({
+      email: 'seattle@test.com',
+      geometry: { type: 'Point', coordinates: [-122.4759902, 47.6147628] }
+    });
+    const miamiDriver = new Driver({
+      email: 'miami@test.com',
+      geometry: { type: 'Point', coordinates: [-80.2534507, 25.791581] }
+    });
+
+    Promise.all([seattleDriver.save(), miamiDriver.save()])
+      .then(() => {
+        request(app)
+          .get('/api/drivers?lng=-80&lat=25')
+          .end((err, response) => {
+            assert(response.body.length === 1);
+            assert(response.body[0].obj.email === 'miami@test.com')
+            done();
+          });
+      });
+  })
 });
